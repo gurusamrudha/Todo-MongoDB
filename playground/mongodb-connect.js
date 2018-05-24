7815b06800111968a1e3194b0efb629620390c9e
Guru <gurusamrudh@gmail.com>
2018-05-24 18:35:52 +0100

    Mongodb Todo first try

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', ( err, db)=>{
+  if(err)
+  {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+
+  console.log('Connected to MongoDB Server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Somthing in DB',
+  //   completed: false
+  // },(err, result) => {
+  //   if(err)
+  //   {
+  //     return console.log('Unable to insert Todo',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Guru samrudha',
+    age: 25,
+    Location: 'India'
+  },(err, result) => {
+    if(err)
+    {
+      return console.log('Unable to insert Users',err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+
+
+});
