1b4a1a31bfcda57253801da74debe1cf4438ec72
Guru <gurusamrudh@gmail.com>
2018-06-03 21:36:56 +0100

    Changed mongoDB password to guru123

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://samrudh:Guru@123@ds111410.mlab.com:11410/mongodbtest' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://samrudh:guru123@ds111410.mlab.com:11410/mongodbtest' || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {mongoose};
